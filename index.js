aed3b3cfc0b108a92d77be0357b7e4ceee20d570
jya2002 <144835748+jya2002@users.noreply.github.com>
2025-05-06 15:17:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Server listening on port ${PORT}`);
-  });
+    console.log(`🚀 Server listening on http://0.0.0.0:${PORT}`);
+});
