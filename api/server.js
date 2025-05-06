0b28cf78c4068735f61fcdb30a5222054eb7b5cb
jya2002 <144835748+jya2002@users.noreply.github.com>
2025-05-06 13:35:17 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,4 +1,9 @@
 // api/server.js
-const app = require('../app'); // Import your Express app
+// index.js
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
 
-module.exports = app; // Export it as a handler for Vercel
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server listening on port ${PORT}`);
+  });
+  
