ff23ed970274bece17fca2d1cbdc69a7f4a5fb74
jya2002 <144835748+jya2002@users.noreply.github.com>
2025-05-07 07:50:45 +0200

    Delete api directory

--- api/server.js
+++
@@ -1,9 +0,0 @@
-// api/server.js
-// index.js
-const app = require('./app');
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Server listening on port ${PORT}`);
-  });
-  
