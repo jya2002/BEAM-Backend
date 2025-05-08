7bab080419927f9db6f91147e4ee32a2a2fc17cd
jya2002 <144835748+jya2002@users.noreply.github.com>
2025-05-08 14:37:08 +0200

    Update 20250508083953-add-refresh-token-version-to-users.js

--- migrations/20250508083953-add-refresh-token-version-to-users.js
+++ migrations/20250508083953-add-refresh-token-version-to-users.js
@@ -1,22 +1,15 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'refreshTokenVersion', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'refreshTokenVersion');
   }
 };
