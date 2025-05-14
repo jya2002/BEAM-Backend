48c7db25f115e5c0ffa82da3cc342396b24ddb5b
jya2002 <144835748+jya2002@users.noreply.github.com>
2025-05-14 20:01:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 // Static files
 console.log('Serving static files from /uploads...');
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Route imports
 console.log('Importing routes...');
