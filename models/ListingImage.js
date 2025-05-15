39a79efb6604a0590fc0bf4e3bdef9ac8b535ac3
jya2002 <144835748+jya2002@users.noreply.github.com>
2025-05-15 13:29:55 +0200

    Update ListingImage.js

--- models/ListingImage.js
+++ models/ListingImage.js
@@ -1,38 +1,38 @@
-// models/ListingImage.js
 module.exports = (sequelize, DataTypes) => {
-    const ListingImage = sequelize.define('ListingImage', {
-      image_id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
+  const ListingImage = sequelize.define('ListingImage', {
+    image_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    listing_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      references: {
+        model: 'Listings', // Matches the table name of the Listing model
+        key: 'listing_id',
       },
-      listing_id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        allowNull: false,
-        references: {
-          model: 'Listings',  // Table name for Listings
-          key: 'listing_id',
-        },
-        onDelete: 'CASCADE',
-      },
-      image_path: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-        comment: 'Path to the image file',
-      },
-    }, {
-      tableName: 'ListingImages',
-      timestamps: false, // We don't need createdAt and updatedAt for images in this case
+      onDelete: 'CASCADE',
+    },
+    image_path: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      comment: 'Relative path to the uploaded image file',
+    },
+  }, {
+    tableName: 'ListingImages',
+    timestamps: false, // Disable createdAt/updatedAt for simplicity
+  });
+
+  // Define association inside associate function
+  ListingImage.associate = (models) => {
+    ListingImage.belongsTo(models.Listing, {
+      foreignKey: 'listing_id',
+      onDelete: 'CASCADE',
     });
-  
-    // Associations
-    ListingImage.associate = (models) => {
-      ListingImage.belongsTo(models.Listing, {
-        foreignKey: 'listing_id',
-        onDelete: 'CASCADE',
-      });
-    };
-  
-    return ListingImage;
   };
+
+  return ListingImage;
+};
+
   
