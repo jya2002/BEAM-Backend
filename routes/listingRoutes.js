e4c085fe599e208f288ba44fed9d761bcf960292
jya2002 <144835748+jya2002@users.noreply.github.com>
2025-05-21 15:53:55 +0200

    Update listingRoutes.js

--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -8,7 +8,11 @@ const uploadMultiple = createUpload('listings').array('images', 5);
 const uploadSingle = createUpload('listings').single('image');
 
 // Routes
-router.post('/listings', uploadMultiple, listingController.createListing);
+router.post('/listings', uploadMultiple, (req, res, next) => {
+  console.log('FIELDS:', req.body);
+  console.log('FILES:', req.files);
+  next();
+}, listingController.createListing);
 router.get('/listings', listingController.getAllListings);
 router.get('/listings/:id', listingController.getListingById);
 router.put('/listings/:id', uploadSingle, listingController.updateListing);
