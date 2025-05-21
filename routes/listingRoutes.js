2f74bf6eeb6aae1e22eafcfaf77d94d3a2835178
jya2002 <144835748+jya2002@users.noreply.github.com>
2025-05-21 18:03:17 +0200

    Update listingRoutes.js

--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -8,11 +8,20 @@ const uploadMultiple = createUpload('listings').array('images', 5);
 const uploadSingle = createUpload('listings').single('image');
 
 // Routes
-router.post('/listings', uploadMultiple, (req, res, next) => {
-  console.log('FIELDS:', req.body);
-  console.log('FILES:', req.files);
-  next();
+router.post('/listings', (req, res, next) => {
+  uploadMultiple(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      console.error('Multer error:', err);
+      return res.status(400).json({ error: err.message });
+    } else if (err) {
+      console.error('Unknown error:', err);
+      return res.status(500).json({ error: err.message });
+    }
+    console.log('Multer passed:', req.files);
+    next();
+  });
 }, listingController.createListing);
+
 router.get('/listings', listingController.getAllListings);
 router.get('/listings/:id', listingController.getListingById);
 router.put('/listings/:id', uploadSingle, listingController.updateListing);
